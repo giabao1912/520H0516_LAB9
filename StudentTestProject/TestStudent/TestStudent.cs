793ec3501c43c28736dad3fa37cefd699054f9de
GiaBao <baocuaai@gmail,com>
2022-11-10 17:34:25 +0700

    add

---
+++ StudentTestProject/TestStudent/TestStudent.cs
@@ -0,0 +1,11 @@
+namespace TestStudent
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Student_v1/Student_v1/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Student_v1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
